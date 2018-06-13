56bbc53520b79003c128f5d1d896cfbda88d90b4
Harshv99i <36041550+Harshv99i@users.noreply.github.com>
2018-06-13 02:31:01 -0700

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,33 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{ 
+int a,b,c;
+printf("Welcome To Calculator \n Enter the task you want to perform \n1: Add \n2: subtract \n3: Multiply\n4: Square\nEnter task number\n ");
+scanf("%d",&a);
+
+if(a==4)
+{
+printf("Enter number");
+scanf("%d",&b);
+b=b*b;
+printf("square is %d",b);
+}
+else
+{
+printf("Enter two numbers ");
+scanf("%d %d",&b,&c);
+
+if(a==1)
+{a=b+c;}
+else if(a==2)
+{a=b-c;}
+else if(a==3)
+{a=b*c;}
+printf(" your answer is %d",a);
+}
+
+
+
+getch();
+}
